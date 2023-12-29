fcbc5eb9ec959d1849b54c7b7141899df978d51f
QuelQuel7 <maciejkucab@duck.com>
2023-12-29 14:05:50 +0100

    Rownanie F.8 W komentarzu

--- Project_GR.py
+++ Project_GR.py
@@ -9,10 +9,15 @@
 import numpy as np 
 
 
+def F8_LHS(r,u):
+# D_u (sqrt(1-u^2) phi^6 K^r_r) + (e^eta/(sqrt(1-u^2))) D_u ((1-u^2)e^(-eta)phi^6 K^phi_phi
 
+    return 0
 
+def F8_RHS(r,u):
+# sqrt(1-u^2)/r^2 D_r (r^2 phi^6 K^r_theta)
 
-
+    return 1
 
 
 def F12_LHS(r):
