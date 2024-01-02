9ce9058c973e1d01ac426647f06903253e965226
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2024-01-02 11:43:43 +0100

    Update Derivatives.py

--- Derivatives.py
+++ Derivatives.py
@@ -13,16 +13,16 @@ class Derivative:
     '''Chebyshev polynomial differentiation matrix.
        Ref.: Trefethen's 'Spectral Methods in MATLAB' book.
     '''
-    x      = cos(pi*arange(0,N+1)/N)
+    x      = np.cos(np.pi*np.arange(0,N+1)/N)
     if N%2 == 0:
         x[N//2] = 0.0 # only when N is even!
-    c      = ones(N+1); c[0] = 2.0; c[N] = 2.0
-    c      = c * (-1.0)**arange(0,N+1)
+    c      = np.ones(N+1); c[0] = 2.0; c[N] = 2.0
+    c      = c * (-1.0)**np.arange(0,N+1)
     c      = c.reshape(N+1,1)
-    X      = tile(x.reshape(N+1,1), (1,N+1))
+    X      = np.tile(x.reshape(N+1,1), (1,N+1))
     dX     = X - X.T
-    D      = dot(c, 1.0/c.T) / (dX+eye(N+1))
-    D      = D - diag( D.sum(axis=1) )
+    D      = np.dot(c, 1.0/c.T) / (dX+np.eye(N+1))
+    D      = D - np.diag( D.sum(axis=1) )
     return D,x
  
 
