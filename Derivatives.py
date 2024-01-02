99716aafb71f5043d2aa884a6b833356389345c6
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2024-01-02 11:41:24 +0100

    Update Derivatives.py
    Cheb function

--- Derivatives.py
+++ Derivatives.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 
 # storage for derivative matrices
@@ -7,6 +8,23 @@ class Derivative:
 
     def __init__(self, size: list) -> None:
         self._matrix = np.zeros(size)
+        
+    def cheb(N):
+    '''Chebushev polynomial differentiation matrix.
+       Ref.: Trefethen's 'Spectral Methods in MATLAB' book.
+    '''
+    x      = cos(pi*arange(0,N+1)/N)
+    if N%2 == 0:
+        x[N//2] = 0.0 # only when N is even!
+    c      = ones(N+1); c[0] = 2.0; c[N] = 2.0
+    c      = c * (-1.0)**arange(0,N+1)
+    c      = c.reshape(N+1,1)
+    X      = tile(x.reshape(N+1,1), (1,N+1))
+    dX     = X - X.T
+    D      = dot(c, 1.0/c.T) / (dX+eye(N+1))
+    D      = D - diag( D.sum(axis=1) )
+    return D,x
+ 
 
 
 class DR(Derivative):
