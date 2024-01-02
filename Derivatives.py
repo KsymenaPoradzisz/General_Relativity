9370c7308d14bb2621cdf273ee3133a5968a59b0
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2024-01-02 12:03:04 +0100

    Update Derivatives.py

--- Derivatives.py
+++ Derivatives.py
@@ -6,7 +6,7 @@ import math
 class Derivative:
     _matrix = None  # static variable to be easily accessible without redundant copying
 
-    def __init__(self, size: tuple) -> None:
+    def __init__(self, size) -> None:
         self._matrix = np.zeros(size)
         self._matrix = self.cheb(size)[0] 
         
