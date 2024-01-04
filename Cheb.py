48020946e6cc8aad88f0d7ff03a7e656d71c9e38
smdrzazga <komoko001@gmail.com>
2024-01-04 14:01:04 +0100

    Add 1d spectral differentiation

---
+++ Cheb.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Grid:
+    grid = None
+
+    def __init__(self, N, mode="cheb") -> None:
+        # allowed modes: "uni" for uniform and "cheb" for Chebyshev grid
+        match mode:
+            case "uni":
+                self.grid = np.array([i/(N-1) for i in range(N)], dtype=np.float64)
+            case "cheb":
+                # Chebyshev polynomial differentiation matrix.
+                #Ref.: Trefethen's 'Spectral Methods in MATLAB' book.
+                
+                # x - N dimensional array of Chebyshev points
+                self.grid = np.array(np.cos(np.pi * np.arange(0, N) / (N-1)), dtype=np.float64)
+                if N % 2 == 1:
+                    self.grid[N//2] = 0.0   # only for N odd!
+            case _:
+                raise ValueError("Choose 'uni' or 'cheb' mode!")
+
+
+                
