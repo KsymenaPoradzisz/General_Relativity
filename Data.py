bc294050db7d7052c6275e93e0928355e57f5c82
Ksymena Poradzisz <73689092+KsymenaPoradzisz@users.noreply.github.com>
2024-01-13 18:42:39 +0100

    you will be executed for writing an insecure code

--- Data.py
+++ Data.py
@@ -14,28 +14,39 @@ class Gauss:
 
     def tg(x):
      #Define compactified variable, L = 2/Pi
-        if(x != 0 and x != 1):
-            return np.tan(np.pi * x/2.) * self.L
-        elif(x == 0):
-            return  1e+6
+        if x > 0 and x < 1:
+            tg = np.tan(np.pi * x/2.) * self.L
+        elif x == 0:
+            tg =  1e+6
+        elif x == 1:
+            tg = -1e+6
         else:
-            return -1e+6
+            raise ValueError("argument should be in range [0,1]")
         return tg
+        
 
     # Define a derivative of such variable, this is Sec^2 * Pi/2 * L, so just Sec^2 but we can have 
    
    # Different values of L, so i leave that
     def secL(x):
-        if(x != 1./2.):
-            return np.pi/2. * 1/(np.cos(np.pi*x/2.))**2 * self.L
-        else: 
-            return 1e+6
+        if x >= 0 and x <= 1:
+            if(x != 1):
+                return np.pi/2. * 1/(np.cos(np.pi*x/2.))**2 * self.L
+            else: 
+                return 1e+6
+        else:
+            raise ValueError("argument should be in range [0,1]")
     #This is the same secans, but without L, because it is convinient for second derivative
     def sec(x):
-        if(x != 1./2.):
-            return 1/(np.cos(np.pi*x/2.))**2
-        else: 
-            return 1e+6
+        if x >= 0 and x <= 1:
+
+            if(x != 1):
+                return 1/(np.cos(np.pi*x/2.))**2
+            else: 
+                return 1e+6
+        else:
+            raise ValueError("argument should be in range [0,1]")
+            
 
 
    #Now define the gauss function up to 3rd derivative
@@ -57,57 +68,67 @@ class Gauss:
         tempe = self.f(x) * self.L * np.pi**3/self.sigma**2
         return ( 1./2. * self.mu * self.sec(x)**2 + (self.L/self.sigma**2)**2 * (self.mu * self.sec(x))**3 - 3./2. * (self.L/self.sigma)**2 * self.sec(x)**3 - 2*self.tg(x) * self.sec(x)**2 + 3 * self.tg(x) * (self.sec(x)/self.sigma)**2 - 3 * self.tg(x) * self.L**2 * self.mu**2 /self.sigma**4 * self.sec(x)**3 + 3*self.tg(x) * (self.L/self.sigma)**2 * self.sec(x)**3 /2. + self.mu * self.sec(x) * self.tg(x)/self.L**2 - 6 * (self.tg(x)/self.sigma)**2 * self.sec(x)**2 + 3 * self.mu * self.tg(x)**2 * self.L**2 /self.sigma**4 * self.sec(x)**3 - self.tg(x)**3 * (self.sec(X)/self.L)**2 +3 * self.tg(x)**3 * (self.sec(x)/self.sigma)*2 - self.tg(x)**3 * self.L**2/self.sigma**4 * self.sec(x)**3) * tempe 
 
+
+
 class Data: 
-    pass
-
-
+    def __init__(self, X: list, U: list) -> None:
+        self.NX = len(X)
+        self.NU = len(U)
 class Eta(Data):
      # for the sake of numerical stability we choose \Eta/R
-     # X - spatial grid; U - angular grid
+   # X - x grid; U - u grid
     def __init__(self, X: list, U: list) -> None:
-        self.eta = self.gaussInit(X, U)
+        super(Eta, self).__init__(X, U)
+        self.eta = np.zeros((self.NX, self.NU))
 
-    def gaussInit(self, X: list, U: list) -> None:
-        NX, NU = len(X), len(U)
-        eta = np.zeros((NX, NU))
-        gaussian = Gauss(sigma = 0.5, mu = 0, epsilon = 0.00001) #example of gauss parameters
-        for i in range(NX):
-            for j in range(NU):
-                u, r = U[j], X[i]
-                f = gaussian.f(r)
-                df = gaussian.fprim(r)
-                dfminus = gaussian.fprim(-r)
-                dfzero = gaussian.fprim(0.)
-                ddf = gaussian.fbis(r)
-                ddfminus = gaussian.fbis(-r)
-                eta[i, j] = -3/2 * (u*u - 1) / (r*r) *(-4.*dfzero + 2.*dfminus + 2.*df + r*ddfminus - r*ddf) if r != 0 else 1e+12 
+    def init_cond(self, mode = "gauss"):
+        match mode:
+            case "gauss":
+                for i in range(self.NX):
+                    for j in range(self.NU):
+                        u = U[j]
+                        r = X[i]
+                        # r = 2./np.pi * np.tan(np.pi * x / 2)
+                        tempGauss = Gauss(sigma = 0.5, mu = 0, epsilon = 0.00001) #example of gauss parameters
+                        f = tempGauss.f(r)
+                        df = tempGauss.fprim(r)
+                        dfminus = tempGauss.fprim(-r)
+                        dfzero = tempGauss.fprim(0.)
+                        ddf = tempGauss.fbis(r)
+                        ddfminus = tempGauss.fbis(-r)
+                        self.eta[j, i] = -3/2 * (u*u - 1) / (r*r) *(-4.*dfzero + 2.*dfminus + 2.*df + r*ddfminus - r*ddf) if r != 0 else 1e+12 
 
-        return np.reshape(eta, -1)
-
+                self.eta = np.reshape(self.eta, -1)
+            case _:
+                raise ValueError("There is no mode you chose. Please choose one existing")   
 
 class Kru(Data):
     # X - spatial grid; U - angular grid
     def __init__(self, X: list, U: list) -> None:
-        self.kru = self.gaussInit(X, U)
+        super(Kru, self).__init__(X, U)
+        self.kru = np.zeros((self.NX,self.NU))
 
-    def gaussInit(self, X: list, U: list) -> None:
-        NX, NU = len(X), len(U)
-        kru = np.zeros((NX,NU))
-        gaussian = Gauss(sigma = 0.5, mu = 0, epsilon = 0.00001) #example of gauss parameters
-        for i in range(NX):
-            for j in range(NU):
-                u, r = U[j], X[i]
-                f = gaussian.fprim(r)
-                df = gaussian.fprim(r)
-                dfminus = gaussian.fprim(-r)
-                ddf = gaussian.fbis(r)
-                ddfzero = gaussian.fbis(0)
-                ddfminus = gaussian.fbis(-r)
-                dddf = gaussian.ftris(r)
-                dddfminus = gaussian.ftris(-r)
-                kru[i, j] = 3./2. * u/(r*r*r) * (-3 * dfminus + 3 * df + r * (-8 * ddf + ddf + ddfminus + r * dddfminus - r * dddf)) if r != 0 else 1e+12 
-        
-        return np.reshape(kru, -1)
+
+    def init_cond(self, mode = "gauss") -> None:
+        match mode:
+            case "gauss":
+                gaussian = Gauss(sigma = 0.5, mu = 0, epsilon = 0.00001) #example of gauss parameters
+                for i in range(NX):
+                    for j in range(NU):
+                        u, r = U[j], X[i]
+                        f = gaussian.fprim(r)
+                        df = gaussian.fprim(r)
+                        dfminus = gaussian.fprim(-r)
+                        ddf = gaussian.fbis(r)
+                        ddfzero = gaussian.fbis(0)
+                        ddfminus = gaussian.fbis(-r)
+                        dddf = gaussian.ftris(r)
+                        dddfminus = gaussian.ftris(-r)
+                        self.kru[i, j] = 3./2. * u/(r*r*r) * (-3 * dfminus + 3 * df + r * (-8 * ddf + ddf + ddfminus + r * dddfminus - r * dddf)) if r != 0 else 1e+12 
+                
+                return np.reshape(self.kru, -1)
+            case _:
+                raise ValueError("There is no mode you chose. Please choose one existing") 
 
 
 class KRTheta(Data):
